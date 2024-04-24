1c87fd126141b6a1ddfb31eadbac4ce09ae9c65f
ThomazEBAC <164674789+ThomazEBAC@users.noreply.github.com>
2024-04-24 16:52:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function fuja(){
+    var botaoNao = document.getElementById("nao")
+
+    var larguraJanela = window.innerWidth;
+    var alturaJanela = window.innerHeight;
+
+    var maxX = larguraJanela - botaoNao.offsetWidth;
+    var maxY = alturaJanela - botaoNao.offsetHeight;
+
+    var aleatorioX = Math.floor(Math.random() * maxX);
+    var aleatorioY = Math.floor(Math.random() * maxY);
+
+    botaoNao.style.left = aleatorioX + "px";
+    botaoNao.style.top = aleatorioY + "px";
+
+}
+
+    const button = document.querySelector('sim')
+    button.addEventListener('click', function(){
+        const audio = document.querySelector('meuAudio')
+        audio.play()
+    })
+
+
